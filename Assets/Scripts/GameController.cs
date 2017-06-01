f9a461576b2792b52375095dd7098eed4fad4fd0
Pavel Gorbach <rgnrok91@gmail.com>
2017-06-01 08:23:42 +0300

    Init commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject rectangle;
+
+    private Camera mainCamera;
+
+    private float rectangleHalfWidth;
+    private float rectangleHalfHeight;
+
+    private int lastRectangleId;
+
+    private float prevClickTime;
+
+    private Rectangle currentRelationRectangle;
+
+    private static GameController instance;
+    public static GameController GetInstance() {
+        if (instance == null) {
+            instance = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+        return instance;
+    }
+
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        mainCamera = Camera.main;
+
+        BoxCollider2D collider = rectangle.GetComponent<BoxCollider2D>();
+        rectangleHalfHeight = collider.size.y / 2;
+        rectangleHalfWidth = collider.size.x / 2;
+
+    }
+
+    void Update() {
+
+        if (Input.GetMouseButtonUp(0)) {
+            //Double click
+            if (Time.time - prevClickTime < 0.3f) {
+                prevClickTime = 0;
+                DestroyRectangleOnHit();
+            }
+            else {
+                prevClickTime = Time.time;
+                if (currentRelationRectangle != null) {
+                    CreateRectangleRelation();
+                }
+                else {
+                    if (CreateRectangle(MousePositionToWord())) {
+                        prevClickTime = 0;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            if (currentRelationRectangle != null) {
+                currentRelationRectangle.ClearRelation();
+                currentRelationRectangle = null;
+            }
+            currentRelationRectangle = GetRectangleOnMousePosition();
+        }
+
+        if (currentRelationRectangle != null) {
+            currentRelationRectangle.DrawRelation(MousePositionToWord());
+        }
+    }
+
+    protected Rectangle GetRectangleOnMousePosition() {
+        RaycastHit2D hit = Physics2D.Raycast(MousePositionToWord(), Vector2.zero);
+        if (hit.collider != null) {
+            return hit.collider.GetComponent<Rectangle>();
+        }
+        return null;
+    }
+
+    protected void CreateRectangleRelation() {
+        Rectangle selectedRectangle = GetRectangleOnMousePosition();
+
+        if (selectedRectangle != null) {
+            currentRelationRectangle.AddChild(selectedRectangle);
+        }
+        else {
+            currentRelationRectangle.ClearRelation();
+        }
+        currentRelationRectangle = null;
+
+    }
+
+    protected bool CreateRectangle(Vector3 position) {
+        if (CanCreateRectangle(position)) {
+            Rectangle rectangleInstance = Instantiate(rectangle, position, Quaternion.identity).GetComponent<Rectangle>();
+            rectangleInstance.Id = lastRectangleId++;
+            return true;
+        }
+        return false;
+    }
+
+    protected bool CanCreateRectangle(Vector3 centerPoint) {
+        RaycastHit2D hit = Physics2D.Raycast(centerPoint, Vector2.zero);
+        if (hit.collider != null) {
+            return false;
+        }
+
+        Vector3 rectangleBoundsOffset = new Vector3(rectangleHalfWidth, rectangleHalfHeight, 0);
+        Collider2D overlap = Physics2D.OverlapArea(centerPoint - rectangleBoundsOffset, centerPoint + rectangleBoundsOffset);
+
+        return overlap == null;
+    }
+
+    protected void DestroyRectangleOnHit() {
+        Rectangle selectedRectangle = GetRectangleOnMousePosition();
+        if (selectedRectangle != null) {
+            selectedRectangle.Destroy();
+        }
+    }
+
+    public Vector3 MousePositionToWord() {
+        Vector3 position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+
+        return position;
+    }
+
+}
---
+++ Assets/Scripts/Rectangle.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using Random = UnityEngine.Random;
+
+public class Rectangle : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public int Id { get; set; }
+
+    //Drag and drop
+    private Vector3 offsetToMouse;
+    private Vector3 prevPosition;
+    private Collider2D[] overlap;
+
+    //Relations
+    private Dictionary<int, Rectangle> childs;
+    private Dictionary<int, Rectangle> parents;
+
+    private LineRenderer lineRender;
+    private BoxCollider2D boxCollider;
+
+    private void Awake() {
+        lineRender = GetComponent<LineRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        childs = new Dictionary<int, Rectangle>();
+        parents = new Dictionary<int, Rectangle>();
+
+        Color32 color = new Color32((byte)Random.Range(0, 256), (byte)Random.Range(0, 256), (byte)Random.Range(0, 256), 255);
+        lineRender.startColor = color;
+        lineRender.endColor = color;
+
+        GetComponent<SpriteRenderer>().color = color;
+    }
+
+
+    public void AddParent(Rectangle relationRectangle) {
+        parents.Add(relationRectangle.Id, relationRectangle);
+    }
+
+    public void AddChild(Rectangle relationRectangle) {
+        DrawRelation(relationRectangle); //Update only new relation
+        childs.Add(relationRectangle.Id, relationRectangle);
+        relationRectangle.AddParent(this);
+    }
+
+    public void DrawParentRelations() {
+        foreach (Rectangle parent in parents.Values) {
+            parent.DrawChildRelations();
+        }
+    }
+
+    public void DrawChildRelations() {
+        lineRender.positionCount = childs.Count * 2;
+        int i = 0;
+        foreach (Rectangle child in childs.Values) {
+            lineRender.SetPosition(i, transform.position);
+            lineRender.SetPosition(i + 1, child.transform.position);
+            i += 2;
+        }
+    }
+
+    //Clear not completed relation
+    public void ClearRelation() {
+        lineRender.positionCount = childs.Count * 2;
+    }
+
+    //Draw not complted relation
+    public void DrawRelation(Vector3 toPosition) {
+        toPosition.z = 0;
+        lineRender.positionCount = childs.Count * 2 + 2;
+        lineRender.SetPosition(lineRender.positionCount - 2, transform.position);
+        lineRender.SetPosition(lineRender.positionCount - 1, toPosition);
+    }
+
+    public void DrawRelation(Rectangle another) {
+        DrawRelation(another.transform.position);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        offsetToMouse = transform.position - GameController.GetInstance().MousePositionToWord();
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        transform.position = GameController.GetInstance().MousePositionToWord() + offsetToMouse;
+
+        overlap = Physics2D.OverlapAreaAll(boxCollider.bounds.min, boxCollider.bounds.max);
+        if (overlap.Length > 1) {
+            transform.position = prevPosition;
+        }
+
+        DrawChildRelations();
+        DrawParentRelations();
+
+        prevPosition = transform.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        offsetToMouse = Vector3.zero;
+    }
+
+    //Destroy rectangle and relations
+    public void Destroy() {
+        foreach (Rectangle parent in parents.Values) {
+            parent.RemoveChild(this);
+        }
+        foreach (Rectangle child in childs.Values) {
+            child.RemoveParent(this);
+        }
+        Destroy(this.gameObject);
+    }
+
+    public void RemoveChild(Rectangle child) {
+        if (childs.ContainsKey(child.Id)) {
+            childs.Remove(child.Id);
+        }
+        DrawChildRelations();
+    }
+
+    public void RemoveParent(Rectangle parent) {
+        if (parents.ContainsKey(parent.Id)) {
+            parents.Remove(parent.Id);
+        }
+    }
+}
